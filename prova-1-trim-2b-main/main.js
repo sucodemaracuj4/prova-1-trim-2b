eb11c5559da9b6f57ffe466667447a1f40e35e1e
su <111879114+sucodemaracuj4@users.noreply.github.com>
2023-08-08 09:59:55 -0300

    Add files via upload

--- prova-1-trim-2b-main/main.js
+++ prova-1-trim-2b-main/main.js
@@ -8,10 +8,10 @@ function searchMusicas() {
     if (musicTitle.includes(userinput)) {
       let atual = musicItems[i];
       atual.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      atual.style.background = '#423a33';
+      atual.style.background = '#6B8E23';
       
       setTimeout(() => {
-        atual.style.background = ''; // Reverte para a cor original
+        atual.style.background = '#d3cca511'// Reverte para a cor original
       }, 1000); // Atraso de 2 segundos (2000 milissegundos)
       
       break;
